b1d1377089fc101db4bab66a4724cbbdd3a012fe
Kevin Sevinche <kevin@kerplunk.local>
2023-11-02 17:01:27 -0500

    Setup I18n

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module Micolet
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.default_locale = :en
+    config.i18n.fallbacks = [:en]
   end
 end
